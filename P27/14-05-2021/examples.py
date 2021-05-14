aed3c2ed4d8ce0f9302fa265f2422e2968dbec18
Jose Garcia <reyes2000jose@gmail.com>
2021-05-14 18:06:56 -0500

    uploading examples

---
+++ P27/14-05-2021/examples.py
@@ -0,0 +1,17 @@
+n = 10
+while n >= 1:
+    print(n)
+    n = n - 1
+
+1 in range(1, 3)
+True
+
+for i in range(1, 11):
+    print(i)
+
+for i in range(1, 11):
+    print("jose")
+
+for i in [True, "jose", "P27"]:
+    if i == "jose":
+        print("es jose")
