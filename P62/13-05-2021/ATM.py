f20c66cb1248a19ffb7ffca6300918a971069633
Jose Garcia <reyes2000jose@gmail.com>
2021-05-13 21:10:14 -0500

    uploading examples

--- P62/13-05-2021/ATM.py
+++ P62/13-05-2021/ATM.py
@@ -1,1 +1,37 @@
 # https://www.codechef.com/problems/HS08TEST
+
+# Pooja would like to withdraw X $US from an ATM. The cash machine will only accept the
+# transaction if X is a multiple of 5, and Pooja's account balance has enough cash to
+# perform the withdrawal transaction (including bank charges). For each successful
+# withdrawal the bank charges 0.50 $US. Calculate Pooja's account balance after an 
+# attempted transaction.
+
+# Input:
+# 30 120.00
+
+# Output:
+# 89.50
+
+# Example - Incorrect Withdrawal Amount (not multiple of 5)
+
+# Input:
+# 42 120.00
+
+# Output:
+# 120.00
+
+# Example - Insufficient Funds
+
+# Input:
+# 300 120.00
+
+# Output:
+# 120.00
+
+# "es divisible" if num % 5 == 0 else "no es divisible"
+
+x = input("Insert the withdrawal: ")
+balance = input("Insert the account's balance: ")
+
+result = balance if not x % 5 == 0 or balance < x + 0.5 else balance - (x + 0.5)
+print(result)
--- P62/13-05-2021/government_program.py
+++ P62/13-05-2021/government_program.py
@@ -1,4 +1,21 @@
 # Determine if a student can access a government subsidy to access to school education. There are 3 requirements:
-# 1. Must be more than 40km to the round of school
+# 1. Must live at less than 40km to the round of school
 # 2. Have more than 2 brothers
 # 3. Family income is lower than 1000 dollars.
+
+distance_to_school = float(input("Insert the distance to the school: "))
+num_brothers_and_sisters = int(input("How many brothers and sisters do you have?: "))
+family_income = float(input("Insert your family income"))
+
+if distance_to_school > 40:
+    print("No")
+elif num_brothers_and_sisters < 2:
+    print("No")
+elif family_income > 1000:
+    print("No")
+else:
+    print("Yes")
+
+# Operadores
+
+# "es divisible" if num % 5 == 0 else "no es divisible"
