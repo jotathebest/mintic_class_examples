311896b86589ede88b0d35935cea2c0f57f8cee2
Jose Garcia <reyes2000jose@gmail.com>
2021-05-21 16:03:18 -0500

    uploading examples

--- P62/21-05-2021/exercise_1.py
+++ P62/21-05-2021/exercise_1.py
@@ -1,3 +1,4 @@
 # Dada una cadena de caracteres, determine cual es el caracter que mas se repite y
-# cuantas veces(case insensitive): ejemplo: input = 'hola soy la letra mas repetida'
+# cuantas veces(case insensitive)
+# ejemplo: input = 'hola soy la letra mas repetida'
 # output = a 5
