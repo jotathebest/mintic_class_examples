3981bb63d710e9f9c13d3bc1f05d23018adc4745
Jose Garcia <reyes2000jose@gmail.com>
2021-05-21 21:43:21 -0500

    uploading examples

--- P62/21-05-2021/exercise_3.py
+++ P62/21-05-2021/exercise_3.py
@@ -1,4 +1,4 @@
-# Imprimir las siguientes figuras usando while y for
+# Imprimir las siguientes figuras usando for y while
 
 # Figura 1
 
@@ -22,3 +22,19 @@
 # ****
 # *****
 # ******
+
+# for i in reversed(range(1, 7)):
+#     print("*" * i)
+
+# for i in range(2, 7):
+#     print("*" * i)
+
+i = 6
+while i > 0:
+    print("*" * i)
+    i -= 1
+
+i = 2
+while i <= 6:
+    print("*" * i)
+    i += 1
