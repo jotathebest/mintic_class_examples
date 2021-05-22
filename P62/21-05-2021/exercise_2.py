450e21715c355b873bb402038bd06bc978ebe60a
Jose Garcia <reyes2000jose@gmail.com>
2021-05-21 21:55:27 -0500

    uploading examples

--- P62/21-05-2021/exercise_2.py
+++ P62/21-05-2021/exercise_2.py
@@ -5,3 +5,10 @@
 
 people = ["Jonas", "Julio", "Mike", "Mez"]
 ages = [25, 30, 31, 39]
+
+length = len(people)
+
+for i in range(0, length):
+    name = people[i]
+    age = ages[i]
+    print(f"{name} {age}")
