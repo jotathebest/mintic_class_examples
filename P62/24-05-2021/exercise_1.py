1e67d11567b919a4929119f0f8034b3957cdb7ac
Jose Garcia <reyes2000jose@gmail.com>
2021-05-24 21:40:39 -0500

    uploading examples

--- P62/24-05-2021/exercise_1.py
+++ P62/24-05-2021/exercise_1.py
@@ -7,3 +7,27 @@
 # (hola carambola (estas cerrando) (muy mal los parentesis --> False
 # (hola carambola) (estas cerrando) (muy bien los parentesis) --> True
 # (hola carambola (estas cerrando (muy bien los parentesis))) --> True
+
+phrase = input("Inserte la frase: ")
+
+# Solution 1
+# contador = 0
+# for letter in phrase:
+#     if letter == "(":
+#         contador += 1
+#     if letter == ")":
+#         contador -= 1
+
+# print("es correcto" if contador == 0 else "es incorrecto")
+
+# Solution 2
+parentesis_abren = 0
+parentesis_cierran = 0
+
+for letter in phrase:
+    if letter == "(":
+        parentesis_abren +=1
+    if letter == ")":
+        parentesis_cierran +=1
+
+print("es correcto" if parentesis_cierran == parentesis_abren else "es incorrecto")
