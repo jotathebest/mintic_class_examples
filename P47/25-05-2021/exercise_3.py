32fe8e50dfed77ea3450ea4a76f108596d829b21
Jose Garcia <reyes2000jose@gmail.com>
2021-05-25 19:56:04 -0500

    uploading examples

--- P47/25-05-2021/exercise_3.py
+++ P47/25-05-2021/exercise_3.py
@@ -2,3 +2,19 @@
 # [1, 2, 3, 4, 5, 4, 3, 7, 9]
 # [1, 2, 3, 4, 5, 7, 9]
 
+def get_unique_elements(my_list):
+    result = []
+    for element in my_list:
+        if element not in result:
+            result.append(element)
+    return result
+
+my_list = [1, 2, 3, 4, 5, 4, 3, 7, 9]
+result = get_unique_elements(my_list)
+print(result)
+
+def get_unique_elements(my_list):
+    return list(set(my_list))
+
+result = get_unique_elements(my_list)
+print(result)
--- P47/25-05-2021/exercise_4.py
+++ P47/25-05-2021/exercise_4.py
@@ -1,1 +1,9 @@
 # https://www.hackerrank.com/challenges/find-a-string/problem
+
+def count_substring(long_str, short_str):
+    counter = 0
+    for l in range(0, len(long_str)):
+        if short_str ==  long_str[l:l + len(short_str)]:
+            counter += 1
+
+    return counter
