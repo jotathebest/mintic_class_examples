364382f157be8b13133e00f673a179c953bf5844
Jose Garcia <reyes2000jose@gmail.com>
2021-05-25 21:28:13 -0500

    uploading examples

--- P62/25-05-2021/exercise_2.py
+++ P62/25-05-2021/exercise_2.py
@@ -1,2 +1,40 @@
 # Write a Python function to find the Max of three numbers.
 
+# def <name> (<arg_1>, <arg_2>, ... <arg_n>):
+#     <algorithm>
+#     [optional] return <result>
+
+# Jose
+def get_max_number(number_1, number_2, number_3):
+    if number_1 >= number_2 and number_1 >= number_3:
+        return number_1
+    if number_2 >= number_1 and number_2 >= number_3:
+        return number_2
+    if number_3 >= number_1 and number_3 >= number_2:
+        return number_3
+
+# number_1 = float(input("Ingrese el número 1: "))
+# number_2 = float(input("Ingrese el número 2: "))
+# number_3 = float(input("Ingrese el número 3: "))
+
+# result = get_max_number(number_1, number_2, number_3)
+# print(f"El número mayor es {result}")
+
+# Roger
+
+def Max_Number(a, b, c):
+    Max_Num= max(a,b,c)
+    print(Max_Num)
+
+A= float(input("Number 1:"))
+B= float(input("Number 2:"))
+C= float(input("Number 3:"))
+
+Max_Number(A, B, C)
+
+# Juan
+def numero_mayor(a,b,c):
+    return f"El numero mayor es {max(a,b,c)}" 
+
+a, b, c = input("Ingrese 3 numeros separados: ").split()
+print(numero_mayor(a,b,c))
--- P62/25-05-2021/exercise_3.py
+++ P62/25-05-2021/exercise_3.py
@@ -2,3 +2,26 @@
 # [1, 2, 3, 4, 5, 4, 3, 7, 9]
 # [1, 2, 3, 4, 5, 7, 9]
 
+
+def pedir_numeros():
+    length = 5
+    my_list = []
+    while length > 0:
+        number = int(input("Inserte el número a agregar: "))
+        my_list.append(number)
+        length -= 1
+    return my_list
+
+def get_unique_elements(my_list):
+    result = []
+    for element in my_list:
+        if element not in result:
+            result.append(element)
+    return result
+
+# my_list = [1, 2, 3, 4, 5, 4, 3, 7, 9]
+my_list = pedir_numeros()
+my_list_2 = [1, 6, 8, 10, 10, 2, 1, 1]
+result = get_unique_elements(my_list)
+result2 = get_unique_elements(my_list_2)
+print(result, result2)
