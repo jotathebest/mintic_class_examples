ec644d07f3f7056274d7a1cb88516ef1cc780857
Jose Garcia <reyes2000jose@gmail.com>
2021-05-26 18:38:30 -0500

    uploading examples

--- P47/25-05-2021/exercise_4.py
+++ P47/25-05-2021/exercise_4.py
@@ -7,3 +7,22 @@ def count_substring(long_str, short_str):
             counter += 1
 
     return counter
+
+# ABCDCDC
+# CDC
+
+# Camilo
+
+def my_finder(main, sub):
+    indexes = []
+    last_index = None
+    for i in range(0, len(main)):
+        if main.find(sub, i) != last_index:
+            last_index = main.find(sub, i)
+            indexes.append(last_index)
+            occurrences = len(indexes) - 1
+    return occurrences
+
+string = str(input("Write a string: "))
+sub_string = str(input("Search for: "))
+print(f"The amount of occurrences for '{sub_string}' in '{string}' is: {my_finder(string, sub_string)}") 
