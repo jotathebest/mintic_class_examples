76499747d08cf44ca7e637bd40ed7612d246ca1b
Jose Garcia <reyes2000jose@gmail.com>
2021-05-25 20:55:26 -0500

    uploading examples

--- P62/25-05-2021/examples.py
+++ P62/25-05-2021/examples.py
@@ -3,3 +3,33 @@
 #     [optional] return <result>
 
 # imprimir los cuadrados del 1 al 5
+
+def print_squares():
+    for i in range(1, 6):
+        print(i ** 2, end=",")
+
+# print_squares()
+
+# imprimir los cuadrados del 1 hasta el número que decida el usuario
+
+def print_squares(upper_limit):
+    for i in range(1, upper_limit + 1):
+        print(i ** 2, end=",")
+
+# number_1 = int(input("Ingrese el numero 1: "))
+# print_squares(number_1)
+# number_2 = int(input("Ingrese el numero 2: "))
+# print_squares(number_2)
+# number_3 = int(input("Ingrese el numero 3: "))
+# print_squares(number_3)
+
+def print_squares(upper_limit):
+    result = []
+    for i in range(1, upper_limit + 1):
+        result.append(i ** 2)
+    return result
+
+number_1 = int(input("Ingrese el numero 1: "))
+squares = print_squares(number_1)
+for element in squares:
+    print(element, end=",")
--- P62/25-05-2021/exercise_1.py
+++ P62/25-05-2021/exercise_1.py
@@ -1,3 +1,25 @@
 # Write a Python program to reverse a string
 # Sample String : "1234abcd"
 # Expected Output : "dcba4321"
+
+# def <name> (<arg_1>, <arg_2>, ... <arg_n>):
+#     <algorithm>
+#     [optional] return <result>
+
+# Jose
+# def reverse_string(phrase):
+#     return phrase[::-1]
+
+# phrase = input("Ingrese la frase: ")
+# result = reverse_string(phrase)
+# print(result)
+
+# Miguel
+def reversa(texto):
+    texto2 = ""
+    for i in reversed(texto):
+        texto2 = texto2 + i
+    print(texto2)
+
+frase = input("Inserte una frase: ")
+reversa(frase) 
