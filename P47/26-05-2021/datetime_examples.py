c65c93a0d03d14243acdbfdf957ee802f463fed7
Jose Garcia <reyes2000jose@gmail.com>
2021-05-26 19:59:11 -0500

    uploading examples

--- P47/26-05-2021/datetime_examples.py
+++ P47/26-05-2021/datetime_examples.py
@@ -7,3 +7,7 @@ date = datetime.datetime.now()
 print(date.year, date.month, date.day, date.hour, date.minute, date.second, date.microsecond)
 
 print(date.strftime("%I"))
+
+date_str = "2021/05/26"
+date = datetime.datetime.strptime(date_str, "%Y/%m/%d")
+print(date.strftime("%Y"))
--- P47/26-05-2021/exercise_2.py
+++ P47/26-05-2021/exercise_2.py
@@ -1,3 +1,18 @@
 # Write a Python program to convert a string to datetime.
 # Sample String : Jan 1 2014 2:43PM
 # Expected Output : 2014-07-01 14:43:00
+
+import datetime
+
+# Solution 1
+def convert_date(date_str):
+    date = datetime.datetime.strptime(date_str, "%b %d %Y %I:%M%p")
+    new_date = date.strftime("%Y-%m-%d %H:%M:%S")
+    return new_date
+
+new_date = convert_date("Jan 1 2014 2:43PM")
+print(new_date)
+
+#Solution 2
+date = datetime.datetime.strptime("Jan 1 2014 2:43PM", "%b %d %Y %I:%M%p")
+print(date)
