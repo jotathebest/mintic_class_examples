fb8cfd1e007be7a002715b51c48ce9f3e49ee4eb
Jose Garcia <reyes2000jose@gmail.com>
2021-05-27 18:48:17 -0500

    uploading examples

--- P47/26-05-2021/exercise_3.py
+++ P47/26-05-2021/exercise_3.py
@@ -2,4 +2,13 @@
 # Sample Date :
 # Current Date : 2015-06-22
 # 5 days before Current Date : 2015-06-17
+import datetime
 
+
+def date_time_delta(date_str, delta_days):
+    date = datetime.datetime.strptime(date_str, "%Y-%m-%d")
+    new_date = date + datetime.timedelta(days=delta_days)
+    return new_date.strftime("%Y-%m-%d")
+
+
+print(date_time_delta("2015-06-22", 5))
--- P47/26-05-2021/exercise_5.py
+++ P47/26-05-2021/exercise_5.py
@@ -1,3 +1,16 @@
 # Write a Python program to get days between two dates. Go to the editor
 # Sample Dates : 2000,2,28, 2001,2,28
 # Expected Output : 366 days, 0:00:00
+
+import datetime
+
+
+def get_number_of_days(date_1, date_2):
+    date_1 = datetime.datetime.strptime(date_1, "%Y,%m,%d")
+    date_2 = datetime.datetime.strptime(date_2, "%Y,%m,%d")
+    result = date_2 - date_1
+    return result.days
+
+
+days = get_number_of_days("2000,2,28", "2001,2,28")
+print(f"{days} days")
