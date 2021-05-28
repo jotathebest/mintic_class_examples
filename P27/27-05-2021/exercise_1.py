52616f400633890ba37644344989cdd1860d3ff9
Jose Garcia <reyes2000jose@gmail.com>
2021-05-28 17:26:19 -0500

    uploading examples

--- P27/27-05-2021/exercise_1.py
+++ P27/27-05-2021/exercise_1.py
@@ -41,6 +41,7 @@ result = [
 # los scripts de abajo serviran para que tengas listas de prueba
 import random
 import time
+import datetime
 
 
 def str_time_prop(start, end, time_format, prop):
@@ -115,20 +116,74 @@ def get_corporal_composition(imc: float):
         return "Obesidad"
 
 
-print(get_corporal_composition(20.1))
-
 # funcion para definir el riesgo
 # args --> composicion_corporal:str, comorbilidades:bool, edad:int
 # return --> risk:bool
+
+# risk que guardara un booleano, en verdadero, si se todas se cumplen:
+#     a. Que la composicion corporal no sea normal
+#     b. Que tenga comorbilidades
+#     c. Que su edad sea mayor a 45 años
+
+
+def get_risk(corporal_comp: str, has_comorbilities: bool, age: int):
+    return (
+        True if corporal_comp != "Normal" and has_comorbilities and age > 45 else False
+    )
+
 
 # funcion para cambiar el formato de la fecha
 # args --> fecha:str [formato mes/dia/año hora:minuto AM/PM]
 # return --> fecha:str [formato año-mes(en letras, completo)-dia hora(formato militar):minuto]
 
+
+def convert_date(date_str):
+    date = datetime.datetime.strptime(date_str, "%m/%d/%Y %I:%M %p")
+    return date.strftime("%Y-%B-%d %H:%M")
+
+
+# print(convert_date("06/28/2008 02:13 AM"))
+
+# construir diccionario
+# args --> composicion_corporal: str, risk: bool, name: str, age: int, fecha: str
+# return --> result:dict
+# dict:
+#  {
+#     "name": "jose garcia",
+#     "age": 30,
+#     "risk": False,
+#     "date": "2020-March-05 23:59",
+#     "corporal_comp": "Normal",
+# }
+
+
+def build_person_dict(corporal_comp: str, risk: bool, name: str, age: int, date: str):
+    person_dict = {
+        "name": name,
+        "age": age,
+        "risk": risk,
+        "date": date,
+        "corporal_comp": corporal_comp,
+    }
+    return person_dict
+
+
 # emparejar cada item de cada lista en un diccionario
 # args --> ages, names, imc, begin_date, comorbilities
 # return --> result_total:list
 
-# construir diccionario
-# args --> composicion_corporal, risk, name, age, fecha
-# return --> result:dict
+
+def process_data(
+    ages: list, names: list, imc: list, begin_date: list, comorbilities: list
+):
+    result = []
+    # iterates the names list with enumerate --> index, name
+    # saves in 4 variables the following information: age, imc, begin_date, comorbility
+    # Create the corporal_comp variable --> get_corporal_comp(imc)
+    # Creates the risk variable --> get_risk(corporal_comp, comorbility, age)
+    # Creates the new_date variable --> convert_date(begin_date)
+    # Creates the person_dict variable --> build_person_dict(risk, new_date, age, name, corporal_comp)
+    # Adds the person_dict to result
+
+    for index, name in enumerate(names):
+        pass
