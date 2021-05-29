574f238cb1d163f5c71d5b85837f5a42c8e0e454
Jose Garcia <reyes2000jose@gmail.com>
2021-05-29 13:04:59 -0500

    uploading examples

---
+++ challenges/challenge_4_template.py
@@ -0,0 +1,23 @@
+def clasificacion_huevos(mi_lista):
+    huevos_AAA = 0
+    huevos_AA = 0
+    huevos_A = 0
+    huevos_BC = 0
+    for huevo in mi_lista:
+        # extrae aca el tipo de huevo con base en el peso (puedes usar una funcion para esto)
+        # incrementa el contador de la categoria del huevo respectiva
+    
+    # conociendo el numero de huevos por categoria, calcula las bandejas por categoria (puedes usar una funcion para esto)
+    bandejas_AAA = 0
+    bandejas_AA = 0
+    bandejas_A = 0
+    bandejas_BC = 0
+
+    # crea el diccionario de resultados con 
+    result = [{'tipo_huevo': 'AAA', 'numero_huevos': huevos_AAA, 'numero_bandejas': bandejas_AAA}
+              {'tipo_huevo': 'AA', 'numero_huevos': huevos_AA, 'numero_bandejas': bandejas_AA}
+              {'tipo_huevo': 'A', 'numero_huevos': huevos_A, 'numero_bandejas': bandejas_A}
+              {'tipo_huevo': 'BC', 'numero_huevos': huevos_BC, 'numero_bandejas': bandejas_BC}
+    ]
+
+    return result
