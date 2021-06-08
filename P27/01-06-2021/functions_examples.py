3a8f6630be0720100435855a24389a52e63873af
Jose Garcia <reyes2000jose@gmail.com>
2021-06-08 18:02:56 -0500

    uploading examples

--- P27/01-06-2021/functions_examples.py
+++ P27/01-06-2021/functions_examples.py
@@ -1,6 +1,5 @@
 # default parameters
 
-
 def fun(a, b, c=1, d=2):
     pass
 
@@ -14,6 +13,31 @@ def func(delta_days=5):
 
 # Create a function that makes a math operation to a given number. By default, it should be to calculate
 # the sqrt
+
+import math
+number = input("inserte numero: ")
+
+def operate_number(number, default_operation="sqrt"):
+    if default_operation == "sqrt":
+        return math.sqrt(number)
+    if default_operation == "tan":
+        return math.tan(number)
+
+def operate_number(number, default_operation=math.sqrt):
+    return default_operation(number)
+
+def sumar(number_1, number_2=1):
+    return number_1 + number_2
+
+MISC_FUNCS = {
+    "tan": math.tan,
+    "sqrt": math.sqrt,
+    "sumar": sumar,
+}
+
+def operate_number(number, default_operation="sqrt"):
+    return MISC_FUNCS[default_operation](number)
+
 
 # Positional arguments
 
@@ -34,3 +58,7 @@ def minimum(*n):
 
 def func(**kwargs):
     print(kwargs)
+
+data = {"name": "jose", "surname": "garcia"}
+def func(name, **kwargs):
+    print(name)
--- P27/01-06-2021/os_module.py
+++ P27/01-06-2021/os_module.py
@@ -1,8 +1,15 @@
+import os
 # Write a Python program to get the name of the operating system (Platform independent), information of current
 # operating system, current working directory, print files and directories in the current directory
 # and raises error in the case of invalid or inaccessible file names and paths.
 
+print(os.uname())
+print(os.getcwd())
+print(os.environ)
+
 # Write a Python program to list only directories, files and all directories, files in a specified path.
+
+print(os.listdir("."))
 
 # Write a Python program to find the parent's process id, real user ID of the current process and
 # change real user ID.
