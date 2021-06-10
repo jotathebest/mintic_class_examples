b3c951c57c10a207180327c91b91bc5367bdf524
Jose Garcia <reyes2000jose@gmail.com>
2021-06-10 18:39:03 -0500

    uploading examples

--- P27/08-06-2021/glob_examples.py
+++ P27/08-06-2021/glob_examples.py
@@ -2,8 +2,27 @@ import glob
 
 # Show paths
 
-paths = glob.glob("./*", recursive=True)
-print(paths)
+# paths = glob.glob("./*", recursive=True)
+# print(paths)
 
-for path in glob.iglob("./*", recursive=True):
-    print(path)
+# for path in glob.iglob("./*", recursive=True):
+#     print(path)
+
+
+with open("/home/jota/repositorios/mintic_class_examples/P27/08-06-2021/data.csv", "r") as f:
+    text = f.read()
+    print(text)
+
+# with open("/home/jota/repositorios/mintic_class_examples/P27/08-06-2021/data.csv", "w") as f:
+#     f.write("Hola Jose")
+
+with open("/home/jota/repositorios/mintic_class_examples/P27/08-06-2021/data.csv", "a") as f:
+    f.write("william,aguilar,test3@test.com")
+
+#windows
+# E:\pyton\ejercicios de clase\datos.csv 
+"documents\\descargas\\archivo.csv"
+
+# name,surname,birthday
+# jose,garcia,01/01/0
+# gonzalo,garcia,01/02/20
--- P47/08-06-2021/glob_examples.py
+++ P47/08-06-2021/glob_examples.py
@@ -2,8 +2,37 @@ import glob
 
 # Show paths
 
-paths = glob.glob("./*", recursive=True)
-print(paths)
+# paths = glob.glob("./*", recursive=True)
+# print(paths)
 
-for path in glob.iglob("./*", recursive=True):
-    print(path)
+# for path in glob.iglob("./*", recursive=True):
+#     print(path)
+
+# with open("/home/jota/repositorios/mintic_class_examples/P47/08-06-2021/data.csv", "r") as f:
+#     text = f.read()
+#     print(text)
+
+# with open("/home/jota/repositorios/mintic_class_examples/P47/08-06-2021/data.csv", "w") as f:
+#     f.write("Hola Jose")
+
+# with open("/home/jota/repositorios/mintic_class_examples/P47/08-06-2021/data.csv", "a") as f:
+#     f.write("gonzalo,garcia,test2@test.com")
+
+# name,surname,hora
+# jose,garcia,6:24
+# carolina,martinez,6:24
+# gonzalo,garcia,6:25
+
+# windows
+# E:\pyton\ejercicios de clase\datos.csv -- > E:\\pyton\\ejercicios de clase\\datos.csv
+
+import datetime
+
+with open("/home/jota/repositorios/mintic_class_examples/P47/08-06-2021/data.csv", "a") as f:
+    f.write("NAME, SURNAME, SCRIPT HOUR\n")
+    run_time_script = datetime.datetime.now().hour
+    f.write(f"Guillermo, Lopez, {run_time_script}\n")
+    run_time_script = datetime.datetime.now().hour
+    f.write(f"Isabel, Chaves, {run_time_script}\n")
+    run_time_script = datetime.datetime.now().hour
+    f.write(f"Alina, Neisa, {run_time_script}\n") 
